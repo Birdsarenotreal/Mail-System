a1741420a343b8900f27f578482afa8f95f32854
Birdsarenotreal <aytrex.real1@gmail.com>
2021-11-22 20:54:37 +0200

    submitted forgotten changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import List from "./components/List.js";
+import Login from "./components/Login.jsx";
+import "./App.css";
+import { React } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="main">
+        <div className="container">
+          <Login></Login>
+          {/* <Route path="/" exact element={<Login />} />
+          <Route path="/signin" element={<Login />} /> */}
+        </div>
+      </div>
+    </Router>
   );
 }
-
 export default App;
